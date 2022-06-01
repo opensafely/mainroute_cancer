470ef96f2f5443cec691e700b0c6925335a58d0e
emmazmi <83311781+emmazmi@users.noreply.github.com>
2022-06-01 12:14:42 +0100

    Add files via upload

---
+++ analysis/codelists.py
@@ -0,0 +1,13 @@
+from cohortextractor import codelist, codelist_from_csv
+
+colorectal_referral_codes = codelist_from_csv(
+    "codelists/phc-2ww-referral-colorectal.csv", system="snomed", column="code"
+)
+
+colorectal_symptom_codes = codelist_from_csv(
+    "codelists/phc-symptoms-colorectal-cancer.csv", system="snomed", column="code"
+)
+
+colorectal_diagnosis_codes = codelist_from_csv(
+    "codelists/phc-colorectal-cancer-icd10.csv", system="icd10", column="code"
+)
