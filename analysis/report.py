16904f65ca66ccbfbb3ee4bc302a0d4925b2e873
emmazmi <83311781+emmazmi@users.noreply.github.com>
2022-06-01 12:22:35 +0100

    Update report.py

--- analysis/report.py
+++ analysis/report.py
@@ -66,101 +66,6 @@ plt.savefig('output/colorectal_cancer_3.jpg')
 
 plt.clf()
 
-data_null = data.notnull()
-
-def condition1(s):
-    if s['colorectal_symptom_1_date_symp_date'] & s['colorectal_symptom_1_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-def condition2(s):
-    if s['colorectal_symptom_2_date_symp_date'] & s['colorectal_symptom_2_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-def condition3(s):
-    if s['colorectal_symptom_3_date_symp_date'] & s['colorectal_symptom_3_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-def condition4(s):
-    if s['colorectal_symptom_4_date_symp_date'] & s['colorectal_symptom_4_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-def condition5(s):
-    if s['colorectal_symptom_5_date_symp_date'] & s['colorectal_symptom_5_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-def condition6(s):
-    if s['colorectal_symptom_6_date_symp_date'] & s['colorectal_symptom_6_date_ref_date']:
-        return 1
-    else:
-        return 0
-
-
-data['colorectal_conversion_1'] = data_null.apply(condition1, axis=1)
-data['colorectal_conversion_2'] = data_null.apply(condition2, axis=1)
-data['colorectal_conversion_3'] = data_null.apply(condition3, axis=1)
-data['colorectal_conversion_4'] = data_null.apply(condition4, axis=1)
-data['colorectal_conversion_5'] = data_null.apply(condition5, axis=1)
-data['colorectal_conversion_6'] = data_null.apply(condition6, axis=1)
-
-data_1 = data[['colorectal_symptom_1_date_symp_date', 'colorectal_conversion_1']]
-data_1 = data_1.rename(columns={"colorectal_symptom_1_date_symp_date":"symptom_date", "colorectal_conversion_1":"conversion"})
-data_1['symptom_date'] = pd.to_datetime(data_1['symptom_date'])
-data_1['symptom_date'] = data_1['symptom_date'].dt.to_period('M')
-
-data_2 = data[['colorectal_symptom_2_date_symp_date', 'colorectal_conversion_2']]
-data_2 = data_2.rename(columns={"colorectal_symptom_2_date_symp_date":"symptom_date", "colorectal_conversion_2":"conversion"})
-data_2['symptom_date'] = pd.to_datetime(data_2['symptom_date'])
-data_2['symptom_date'] = data_2['symptom_date'].dt.to_period('M')
-
-data_3 = data[['colorectal_symptom_3_date_symp_date', 'colorectal_conversion_3']]
-data_3 = data_3.rename(columns={"colorectal_symptom_3_date_symp_date":"symptom_date", "colorectal_conversion_3":"conversion"})
-data_3['symptom_date'] = pd.to_datetime(data_3['symptom_date'])
-data_3['symptom_date'] = data_3['symptom_date'].dt.to_period('M')
-
-data_4 = data[['colorectal_symptom_4_date_symp_date', 'colorectal_conversion_4']]
-data_4 = data_4.rename(columns={"colorectal_symptom_4_date_symp_date":"symptom_date", "colorectal_conversion_4":"conversion"})
-data_4['symptom_date'] = pd.to_datetime(data_4['symptom_date'])
-data_4['symptom_date'] = data_4['symptom_date'].dt.to_period('M')
-
-data_5 = data[['colorectal_symptom_5_date_symp_date', 'colorectal_conversion_5']]
-data_5 = data_5.rename(columns={"colorectal_symptom_5_date_symp_date":"symptom_date", "colorectal_conversion_5":"conversion"})
-data_5['symptom_date'] = pd.to_datetime(data_5['symptom_date'])
-data_5['symptom_date'] = data_5['symptom_date'].dt.to_period('M')
-
-data_6 = data[['colorectal_symptom_6_date_symp_date', 'colorectal_conversion_6']]
-data_6 = data_6.rename(columns={"colorectal_symptom_6_date_symp_date":"symptom_date", "colorectal_conversion_6":"conversion"})
-data_6['symptom_date'] = pd.to_datetime(data_6['symptom_date'])
-data_6['symptom_date'] = data_6['symptom_date'].dt.to_period('M')
-
-data_symptom_conversion = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6])
-
-unique_dates = data_symptom_conversion['symptom_date'].unique()
-
-unique_dates = sorted(unique_dates)
-
-dict = {}
-
-for item in unique_dates:
-    df = data_symptom_conversion.loc[data_symptom_conversion['symptom_date'] == item]
-    con = df['conversion'].mean()
-    dict.update({item: con})
-
-plt.bar(range(len(dict)), list(dict.values()), tick_label=list(dict.keys()))
-
-plt.savefig('output/colorectal_cancer_4.jpg')
-
-plt.clf()
-
 data_diagnosis['colorectal_symptom_1_date_diag_date'] = pd.to_datetime(data_diagnosis['colorectal_symptom_1_date_diag_date'])
 data_diagnosis['colorectal_symptom_2_date_diag_date'] = pd.to_datetime(data_diagnosis['colorectal_symptom_2_date_diag_date'])
 data_diagnosis['colorectal_symptom_3_date_diag_date'] = pd.to_datetime(data_diagnosis['colorectal_symptom_3_date_diag_date'])
@@ -182,4 +87,4 @@ counts_diag = counts_diag.sort_index()
 
 ax = counts_diag.plot.bar()
 
-plt.savefig('output/colorectal_cancer_5.jpg')
+plt.savefig('output/colorectal_cancer_4.jpg')
