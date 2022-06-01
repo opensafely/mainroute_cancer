b717fe06ce233a14f1353946f879068e1088d365
emmazmi <83311781+emmazmi@users.noreply.github.com>
2022-06-01 12:14:12 +0100

    Delete codelists.py

--- analysis/codelists.py
+++
@@ -1,9 +0,0 @@
-from cohortextractor import codelist, codelist_from_csv
-
-colorectal_referral_codes = codelist_from_csv(
-    "codelists/phc-2ww-referral-colorectal.csv", system="snomed", column="code"
-)
-
-colorectal_symptom_codes = codelist_from_csv(
-    "codelists/phc-symptoms-colorectal-cancer.csv", system="snomed", column="code"
-)
