9f29882ecdbaaa4695a4ef9a62ebd454bc595b58
emmazmi <83311781+emmazmi@users.noreply.github.com>
2022-09-08 00:28:02 +0100

    Add files via upload

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -19,3 +19,23 @@ colorectal_diagnosis_codes_snomed = codelist_from_csv(
 colorectal_diagnosis_codes_read = codelist_from_csv(
     "codelists/phc-phc-colorectal-cancer-ctv3.csv", system="ctv3", column="code"
 )
+
+anaemia_codes = codelist_from_csv(
+    "codelists/phc-symptom-colorectal-anaemia.csv", system="snomed", column="code"
+)
+
+cibh_codes = codelist_from_csv(
+    "codelists/phc-symptom-colorectal-cibh.csv", system="snomed", column="code"
+)
+
+prbleeding_codes = codelist_from_csv(
+    "codelists/phc-symptom-colorectal-pr-bleeding.csv", system="snomed", column="code"
+)
+
+wl_codes = codelist_from_csv(
+    "codelists/phc-symptom-colorectal-wl.csv", system="snomed", column="code"
+)
+
+fit_codes = codelist_from_csv(
+    "codelists/phc-fit-test.csv", system="snomed", column="code"
+)
