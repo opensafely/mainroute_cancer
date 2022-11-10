5d470bb9880a886cc3f0781a31cc79c616920468
emmazmi <83311781+emmazmi@users.noreply.github.com>
2022-11-10 20:32:45 +0000

    Add files via upload

--- analysis/report_2wwcolorectal.py
+++ analysis/report_2wwcolorectal.py
@@ -5,18 +5,18 @@ import numpy as np
 data = pd.read_csv("output/input_2wwcolorectal.csv")
 
 ########## diagnosis conversion plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-def condition1(s):
-    if s['colorectal_referral_date'] & s['colorectal_diagnosis_date']:
+"""def condition1(s):
+    if ~s['colorectal_referral_date'].isna() & ~s['colorectal_diagnosis_date'].isna():
         return 1
     else:
         return 0
 
-data['colorectal_conversion_1'] = data_null.apply(condition1, axis=1)
+data['colorectal_conversion_1'] = data.apply(condition1, axis=1)"""
 
-data_referral_conversion = data[['colorectal_referral_date', 'colorectal_conversion_1']]
-data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_conversion_1":"conversion"})
+data_referral_conversion = data[['colorectal_referral_date', 'colorectal_diagnosis_flag']]
+data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_diagnosis_flag":"conversion"})
 data_referral_conversion['referral_date'] = pd.to_datetime(data_referral_conversion['referral_date'])
 data_referral_conversion['referral_date'] = data_referral_conversion['referral_date'].dt.to_period('M')
 
@@ -38,18 +38,18 @@ plt.savefig('output/colorectal_cancer_10.jpg')
 plt.clf()
 
 ########## FIT test plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-def condition2(s):
-    if s['colorectal_referral_date'] & s['fit_date']:
+"""def condition2(s):
+    if ~s['colorectal_referral_date'].isna() & ~s['fit_date'].isna():
         return 1
     else:
         return 0
 
-data['colorectal_fit_rate'] = data_null.apply(condition2, axis=1)
+data['colorectal_fit_rate'] = data.apply(condition2, axis=1)"""
 
-data_fit_rate = data[['colorectal_referral_date', 'colorectal_fit_rate']]
-data_fit_rate = data_fit_rate.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_fit_rate":"fit_rate"})
+data_fit_rate = data[['colorectal_referral_date', 'fit_flag']]
+data_fit_rate = data_fit_rate.rename(columns={"colorectal_referral_date":"referral_date", "fit_flag":"fit_rate"})
 data_fit_rate['referral_date'] = pd.to_datetime(data_fit_rate['referral_date'])
 data_fit_rate['referral_date'] = data_fit_rate['referral_date'].dt.to_period('M')
 
@@ -71,20 +71,20 @@ plt.savefig('output/colorectal_cancer_11.jpg')
 plt.clf()
 
 ########## anaemia symptom plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-data.loc[data["age"]<60, "anaemia_symptom_date"] = ""
+#data.loc[data["age"]<60, "anaemia_symptom_date"] = ""
 
 def condition3(s):
-    if s['colorectal_referral_date'] & s['anaemia_symptom_date']:
+    if (s['anaemia_symptom_flag']==1) & (s['age']>=60):
         return 1
     else:
         return 0
 
-data['colorectal_anaemia_rate'] = data_null.apply(condition3, axis=1)
+data['anaemia_symptom_flag2'] = data.apply(condition3, axis=1)
 
-data_anaemia_rate = data[['colorectal_referral_date', 'colorectal_anaemia_rate']]
-data_anaemia_rate = data_anaemia_rate.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_anaemia_rate":"anaemia_rate"})
+data_anaemia_rate = data[['colorectal_referral_date', 'anaemia_symptom_flag2']]
+data_anaemia_rate = data_anaemia_rate.rename(columns={"colorectal_referral_date":"referral_date", "anaemia_symptom_flag2":"anaemia_rate"})
 data_anaemia_rate['referral_date'] = pd.to_datetime(data_anaemia_rate['referral_date'])
 data_anaemia_rate['referral_date'] = data_anaemia_rate['referral_date'].dt.to_period('M')
 
@@ -106,20 +106,20 @@ plt.savefig('output/colorectal_cancer_12.jpg')
 plt.clf()
 
 ########## CIBH symptom plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-data.loc[data["age"]<60, "cibh_symptom_date"] = ""
+#data.loc[data["age"]<60, "cibh_symptom_date"] = ""
 
 def condition4(s):
-    if s['colorectal_referral_date'] & s['cibh_symptom_date']:
+    if (s['cibh_symptom_flag']==1) & (s['age']>=60):
         return 1
     else:
         return 0
 
-data['colorectal_cibh_rate'] = data_null.apply(condition4, axis=1)
+data['cibh_symptom_flag2'] = data.apply(condition4, axis=1)
 
-data_cibh_rate = data[['colorectal_referral_date', 'colorectal_cibh_rate']]
-data_cibh_rate = data_cibh_rate.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_cibh_rate":"cibh_rate"})
+data_cibh_rate = data[['colorectal_referral_date', 'cibh_symptom_flag2']]
+data_cibh_rate = data_cibh_rate.rename(columns={"colorectal_referral_date":"referral_date", "cibh_symptom_flag2":"cibh_rate"})
 data_cibh_rate['referral_date'] = pd.to_datetime(data_cibh_rate['referral_date'])
 data_cibh_rate['referral_date'] = data_cibh_rate['referral_date'].dt.to_period('M')
 
@@ -141,20 +141,20 @@ plt.savefig('output/colorectal_cancer_13.jpg')
 plt.clf()
 
 ########## PR bleeding symptom plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-data.loc[data["age"]<50, "prbleeding_symptom_date"] = ""
+#data.loc[data["age"]<50, "prbleeding_symptom_date"] = ""
 
 def condition5(s):
-    if s['colorectal_referral_date'] & s['prbleeding_symptom_date']:
+    if (s['prbleeding_symptom_flag']==1) & (s['age']>=50):
         return 1
     else:
         return 0
 
-data['colorectal_prbleeding_rate'] = data_null.apply(condition5, axis=1)
+data['prbleeding_symptom_flag2'] = data.apply(condition5, axis=1)
 
-data_prbleeding_rate = data[['colorectal_referral_date', 'colorectal_prbleeding_rate']]
-data_prbleeding_rate = data_prbleeding_rate.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_prbleeding_rate":"prbleeding_rate"})
+data_prbleeding_rate = data[['colorectal_referral_date', 'prbleeding_symptom_flag2']]
+data_prbleeding_rate = data_prbleeding_rate.rename(columns={"colorectal_referral_date":"referral_date", "prbleeding_symptom_flag2":"prbleeding_rate"})
 data_prbleeding_rate['referral_date'] = pd.to_datetime(data_prbleeding_rate['referral_date'])
 data_prbleeding_rate['referral_date'] = data_prbleeding_rate['referral_date'].dt.to_period('M')
 
@@ -176,20 +176,20 @@ plt.savefig('output/colorectal_cancer_14.jpg')
 plt.clf()
 
 ########## weight loss symptom plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-data.loc[data["age"]<40, "wl_symptom_date"] = ""
+#data.loc[data["age"]<40, "wl_symptom_date"] = ""
 
 def condition6(s):
-    if s['colorectal_referral_date'] & s['wl_symptom_date']:
+    if (s['wl_symptom_flag']==1) & (s['age']>=40):
         return 1
     else:
         return 0
 
-data['colorectal_wl_rate'] = data_null.apply(condition6, axis=1)
+data['wl_symptom_flag2'] = data.apply(condition6, axis=1)
 
-data_wl_rate = data[['colorectal_referral_date', 'colorectal_wl_rate']]
-data_wl_rate = data_wl_rate.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_wl_rate":"wl_rate"})
+data_wl_rate = data[['colorectal_referral_date', 'wl_symptom_flag2']]
+data_wl_rate = data_wl_rate.rename(columns={"colorectal_referral_date":"referral_date", "wl_symptom_flag2":"wl_rate"})
 data_wl_rate['referral_date'] = pd.to_datetime(data_wl_rate['referral_date'])
 data_wl_rate['referral_date'] = data_wl_rate['referral_date'].dt.to_period('M')
 
@@ -211,14 +211,16 @@ plt.savefig('output/colorectal_cancer_15.jpg')
 plt.clf()
 
 ########## time to diagnosis plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
-data['colorectal_diagnosis_date'] = pd.to_datetime(data['colorectal_diagnosis_date'])
-data['colorectal_referral_date'] = pd.to_datetime(data['colorectal_referral_date'])
+data_time_to_diagnosis = data[data['colorectal_diagnosis_flag']==1]
 
-data['timetodiag'] = data['colorectal_diagnosis_date'] - data['colorectal_referral_date']
+data_time_to_diagnosis['colorectal_diagnosis_date'] = pd.to_datetime(data_time_to_diagnosis['colorectal_diagnosis_date'])
+data_time_to_diagnosis['colorectal_referral_date'] = pd.to_datetime(data_time_to_diagnosis['colorectal_referral_date'])
 
-data['timetodiag'] = data['timetodiag'].dt.days
+data_time_to_diagnosis['timetodiag'] = data_time_to_diagnosis['colorectal_diagnosis_date'] - data_time_to_diagnosis['colorectal_referral_date']
+
+data_time_to_diagnosis['timetodiag'] = data_time_to_diagnosis['timetodiag'].dt.days
 
 def year(x):
     if (x >= pd.to_datetime('2019-03-23')) & (x < pd.to_datetime('2020-03-23')):
@@ -228,11 +230,11 @@ def year(x):
     elif (x >= pd.to_datetime('2021-03-23')) & (x < pd.to_datetime('2022-03-23')):
         return 3
 
-data['timecovid'] = data['colorectal_referral_date'].apply(year)
+data_time_to_diagnosis['timecovid'] = data_time_to_diagnosis['colorectal_referral_date'].apply(year)
 
-data_precovid = data[data['timecovid'] == 1]
-data_yr1covid = data[data['timecovid'] == 2]
-data_yr2covid = data[data['timecovid'] == 3]
+data_precovid = data_time_to_diagnosis[data_time_to_diagnosis['timecovid'] == 1]
+data_yr1covid = data_time_to_diagnosis[data_time_to_diagnosis['timecovid'] == 2]
+data_yr2covid = data_time_to_diagnosis[data_time_to_diagnosis['timecovid'] == 3]
 
 times_precovid = data_precovid['timetodiag'].to_numpy()
 times_yr1covid = data_yr1covid['timetodiag'].to_numpy()
@@ -266,20 +268,14 @@ plt.savefig('output/colorectal_cancer_16.jpg')
 plt.clf()
 
 ########## diagnosis conversion <40 plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
 data_40 = data[data['age']<40]
 
-def condition7(s):
-    if s['colorectal_referral_date'] & s['colorectal_diagnosis_date']:
-        return 1
-    else:
-        return 0
+#data_40['colorectal_conversion_2'] = data_40.apply(condition1, axis=1)
 
-data_40['colorectal_conversion_2'] = data_null.apply(condition7, axis=1)
-
-data_referral_conversion = data_40[['colorectal_referral_date', 'colorectal_conversion_2']]
-data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_conversion_2":"conversion"})
+data_referral_conversion = data_40[['colorectal_referral_date', 'colorectal_diagnosis_flag']]
+data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_diagnosis_flag":"conversion"})
 data_referral_conversion['referral_date'] = pd.to_datetime(data_referral_conversion['referral_date'])
 data_referral_conversion['referral_date'] = data_referral_conversion['referral_date'].dt.to_period('M')
 
@@ -301,20 +297,14 @@ plt.savefig('output/colorectal_cancer_17.jpg')
 plt.clf()
 
 ########## diagnosis conversion 40-60 plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
 data_60 = data[(data['age']>=40) & (data['age']<60)]
 
-def condition8(s):
-    if s['colorectal_referral_date'] & s['colorectal_diagnosis_date']:
-        return 1
-    else:
-        return 0
+#data_60['colorectal_conversion_3'] = data_60.apply(condition1, axis=1)
 
-data_60['colorectal_conversion_3'] = data_null.apply(condition8, axis=1)
-
-data_referral_conversion = data_60[['colorectal_referral_date', 'colorectal_conversion_3']]
-data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_conversion_3":"conversion"})
+data_referral_conversion = data_60[['colorectal_referral_date', 'colorectal_diagnosis_flag']]
+data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_diagnosis_flag":"conversion"})
 data_referral_conversion['referral_date'] = pd.to_datetime(data_referral_conversion['referral_date'])
 data_referral_conversion['referral_date'] = data_referral_conversion['referral_date'].dt.to_period('M')
 
@@ -336,20 +326,14 @@ plt.savefig('output/colorectal_cancer_18.jpg')
 plt.clf()
 
 ########## diagnosis conversion 60-80 plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
 data_80 = data[(data['age']>=60) & (data['age']<80)]
 
-def condition9(s):
-    if s['colorectal_referral_date'] & s['colorectal_diagnosis_date']:
-        return 1
-    else:
-        return 0
+#data_80['colorectal_conversion_4'] = data_80.apply(condition1, axis=1)
 
-data_80['colorectal_conversion_4'] = data_null.apply(condition9, axis=1)
-
-data_referral_conversion = data_80[['colorectal_referral_date', 'colorectal_conversion_4']]
-data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_conversion_4":"conversion"})
+data_referral_conversion = data_80[['colorectal_referral_date', 'colorectal_diagnosis_flag']]
+data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_diagnosis_flag":"conversion"})
 data_referral_conversion['referral_date'] = pd.to_datetime(data_referral_conversion['referral_date'])
 data_referral_conversion['referral_date'] = data_referral_conversion['referral_date'].dt.to_period('M')
 
@@ -371,20 +355,14 @@ plt.savefig('output/colorectal_cancer_19.jpg')
 plt.clf()
 
 ########## diagnosis conversion 80+ plot ##########
-data_null = data.notnull()
+#data_null = data.notnull()
 
 data_80plus = data[data['age']>=80]
 
-def condition10(s):
-    if s['colorectal_referral_date'] & s['colorectal_diagnosis_date']:
-        return 1
-    else:
-        return 0
+#data_80plus['colorectal_conversion_5'] = data_80plus.apply(condition1, axis=1)
 
-data_80plus['colorectal_conversion_5'] = data_null.apply(condition10, axis=1)
-
-data_referral_conversion = data_80plus[['colorectal_referral_date', 'colorectal_conversion_5']]
-data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_conversion_5":"conversion"})
+data_referral_conversion = data_80plus[['colorectal_referral_date', 'colorectal_diagnosis_flag']]
+data_referral_conversion = data_referral_conversion.rename(columns={"colorectal_referral_date":"referral_date", "colorectal_diagnosis_flag":"conversion"})
 data_referral_conversion['referral_date'] = pd.to_datetime(data_referral_conversion['referral_date'])
 data_referral_conversion['referral_date'] = data_referral_conversion['referral_date'].dt.to_period('M')
 
--- analysis/study_definition_2wwcolorectal.py
+++ analysis/study_definition_2wwcolorectal.py
@@ -33,12 +33,26 @@ study = StudyDefinition(
         find_first_match_in_period=True,
     ),
 
+    colorectal_diagnosis_flag=patients.with_these_clinical_events(
+        colorectal_diagnosis_codes_snomed,
+        between=["colorectal_referral_date", "colorectal_referral_date + 122 days"],
+        returning='binary_flag',
+        return_binary_flag=None,
+    ),
+
     anaemia_symptom_date=patients.with_these_clinical_events(
         anaemia_codes,
         between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
         returning="date",
         date_format="YYYY-MM-DD",
         find_last_match_in_period=True,
+    ),
+
+    anaemia_symptom_flag=patients.with_these_clinical_events(
+        anaemia_codes,
+        between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
+        returning='binary_flag',
+        return_binary_flag=None,
     ),
 
     cibh_symptom_date=patients.with_these_clinical_events(
@@ -49,12 +63,26 @@ study = StudyDefinition(
         find_last_match_in_period=True,
     ),
 
+    cibh_symptom_flag=patients.with_these_clinical_events(
+        cibh_codes,
+        between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
+        returning='binary_flag',
+        return_binary_flag=None,
+    ),
+
     prbleeding_symptom_date=patients.with_these_clinical_events(
         prbleeding_codes,
         between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
         returning="date",
         date_format="YYYY-MM-DD",
         find_last_match_in_period=True,
+    ),
+
+    prbleeding_symptom_flag=patients.with_these_clinical_events(
+        prbleeding_codes,
+        between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
+        returning='binary_flag',
+        return_binary_flag=None,
     ),
 
     wl_symptom_date=patients.with_these_clinical_events(
@@ -65,12 +93,26 @@ study = StudyDefinition(
         find_last_match_in_period=True,
     ),
 
+    wl_symptom_flag=patients.with_these_clinical_events(
+        wl_codes,
+        between=["colorectal_referral_date - 30 days", "colorectal_referral_date"],
+        returning='binary_flag',
+        return_binary_flag=None,
+    ),
+
     fit_date=patients.with_these_clinical_events(
         fit_codes,
         between=["colorectal_referral_date - 60 days", "colorectal_referral_date"],
         returning="date",
         date_format="YYYY-MM-DD",
         find_last_match_in_period=True,
+    ),
+
+    fit_flag=patients.with_these_clinical_events(
+        fit_codes,
+        between=["colorectal_referral_date - 60 days", "colorectal_referral_date"],
+        returning='binary_flag',
+        return_binary_flag=None,
     ),
 
     age=patients.age_as_of(
