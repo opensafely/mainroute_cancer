7a0f240eb9d0194c53c5c4207c85e610780815b5
emmazmi <83311781+emmazmi@users.noreply.github.com>
2023-11-06 23:21:27 +0000

    Add files via upload

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,5 +1,18 @@
 from cohortextractor import codelist, codelist_from_csv
 
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+ethnicity_codes_16 = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_16",
+)
+
 colorectal_referral_codes = codelist_from_csv(
     "codelists/phc-2ww-referral-colorectal.csv", system="snomed", column="code"
 )
@@ -20,8 +33,8 @@ colorectal_diagnosis_codes_read = codelist_from_csv(
     "codelists/phc-phc-colorectal-cancer-ctv3.csv", system="ctv3", column="code"
 )
 
-anaemia_codes = codelist_from_csv(
-    "codelists/phc-symptom-colorectal-anaemia.csv", system="snomed", column="code"
+ida_codes = codelist_from_csv(
+    "codelists/phc-symptom-colorectal-ida.csv", system="snomed", column="code"
 )
 
 cibh_codes = codelist_from_csv(
@@ -34,6 +47,18 @@ prbleeding_codes = codelist_from_csv(
 
 wl_codes = codelist_from_csv(
     "codelists/phc-symptom-colorectal-wl.csv", system="snomed", column="code"
+)
+
+abdomass_codes = codelist_from_csv(
+    "codelists/phc-symptom-lowergi-abdo-mass.csv", system="snomed", column="code"
+)
+
+abdopain_codes = codelist_from_csv(
+    "codelists/phc-symptom-lowergi-abdo-pain.csv", system="snomed", column="code"
+)
+
+anaemia_codes = codelist_from_csv(
+    "codelists/phc-symptom-lowergi-anaemia.csv", system="snomed", column="code"
 )
 
 fit_codes = codelist_from_csv(
