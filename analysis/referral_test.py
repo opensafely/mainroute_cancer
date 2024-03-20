7e60ec2c3513abb0e8c6adf16754e27587318bc5
emmazmi <83311781+emmazmi@users.noreply.github.com>
2024-03-20 23:31:21 +0000

    update referral test

--- analysis/referral_test.py
+++ analysis/referral_test.py
@@ -27,7 +27,7 @@ dataset.lowerGI_2ww_date = lowerGI_2ww_ref.date
 first_attendance_code = ["1", "3"]
 colorectal_surg_clinic_code = ["104"]
 gastro_clinic_code = ["301"]
-#colonoscopy_code = ["H22", "H18", "H25", "H28"]
+opcs_colonoscopy_code = ["H22", "H18", "H25", "H28"]
 
 """
 colorectal_surg_clinic_21days = (
@@ -92,21 +92,20 @@ colonoscopy_21days = (
             opa_proc.appointment_date
         ).first_for_patient()
 )
-
-colonoscopy_1month = (
-    opa_proc.where(opa_proc.primary_procedure_code.is_in(colonoscopy_code)
+"""
+opa_colonoscopy_6weeks = (
+    opa_proc.where(opa_proc.primary_procedure_code.is_in(opcs_colonoscopy_code)
         ).where(
-            opa_proc.appointment_date.is_on_or_between(dataset.lowerGI_2ww_date, dataset.lowerGI_2ww_date + months(1))
+            opa_proc.appointment_date.is_on_or_between(dataset.lowerGI_2ww_date, dataset.lowerGI_2ww_date + days(42))
         ).sort_by(
             opa_proc.appointment_date
         ).first_for_patient()
 )
 
-dataset.colonoscopy_21d = colonoscopy_21days.exists_for_patient()
-dataset.colonoscopy_1m = colonoscopy_1month.exists_for_patient()
-"""
+#dataset.colonoscopy_21d = colonoscopy_21days.exists_for_patient()
+dataset.opa_colonoscopy_6w = opa_colonoscopy_6weeks.exists_for_patient()
 
-lowergi_diagnostic_6weeks = (
+apcs_diagnostic_6weeks = (
     apcs.where(apcs.spell_core_hrg_sus.is_in(codelists.lowerGI_diagnostic_codes)
         ).where(
             apcs.admission_date.is_on_or_between(dataset.lowerGI_2ww_date, dataset.lowerGI_2ww_date + days(42))
@@ -115,7 +114,20 @@ lowergi_diagnostic_6weeks = (
         ).first_for_patient()
 )
 
-dataset.lowergi_diagnostic_6w = lowergi_diagnostic_6weeks.exists_for_patient()
+dataset.apcs_diagnostic_6w = apcs_diagnostic_6weeks.exists_for_patient()
+
+opa_diagnostic_6weeks = (
+    opa.where(opa.hrg_code.is_in(codelists.lowerGI_diagnostic_codes)
+        ).where(
+            opa.appointment_date.is_on_or_between(dataset.lowerGI_2ww_date, dataset.lowerGI_2ww_date + days(42))
+        ).sort_by(
+            opa.appointment_date
+        ).first_for_patient()
+)
+
+dataset.opa_diagnostic_6w = opa_diagnostic_6weeks.exists_for_patient()
+
+dataset.lowergi_diagnostic_6w = (dataset.opa_colonoscopy_6w | dataset.apcs_diagnostic_6w | dataset.opa_diagnostic_6w)
 
 opa_1month = (
     opa.where(opa.appointment_date.is_on_or_between(dataset.lowerGI_2ww_date, dataset.lowerGI_2ww_date + months(1))
